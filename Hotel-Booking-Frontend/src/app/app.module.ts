dbf2c07dc745e9b36ea2b18f1d27687e479deca6
sergiomeneguzzo <sergio03.mene@gmail.com>
2024-10-15 17:07:14 +0200

    Create Frontend

---
+++ Hotel-Booking-Frontend/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatCard, MatCardContent, MatCardFooter} from '@angular/material/card';
+import {NgOptimizedImage} from '@angular/common';
+import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
+import {MatInput, MatInputModule} from '@angular/material/input';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RegisterComponent } from './pages/auth/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    MatCard,
+    NgOptimizedImage,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatIconButton,
+    MatCardFooter,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
