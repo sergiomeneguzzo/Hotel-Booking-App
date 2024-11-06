9f1a11b5838cc641bd12406e8b783d24f535a255
sergiomeneguzzo <sergio03.mene@gmail.com>
2024-11-06 17:32:11 +0100

    .

--- Hotel-Booking-Backend/src/api/services/email.service.ts
+++ Hotel-Booking-Backend/src/api/services/email.service.ts
@@ -114,7 +114,7 @@ export const sendConfirmationEmail = async (email: string, userId: string) => {
         tuo indirizzo email cliccando sul pulsante qui sotto.
       </p>
 
-      <a href="http://localhost:4200/email-confirmed?token=${token}" class="btn-confirm">
+      <a href="https://hotel-booking-app-sigma-three.vercel.app/email-confirmed?token=${token}" class="btn-confirm">
         Completa Registrazione
       </a>
 
